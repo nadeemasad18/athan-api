0ccedbebfcca017019e15696cdadbe9593964882
Nadeem Asad <nadeemasad18@gmail.com>
2022-11-15 13:43:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+import React,{useState} from 'react'
+
+import AthanSearch from './components/AthanSearch';
+
+
+
+function App() {
+
+
+
+  return (
+
+
+<body>
+      
+      <h1 >Athanify</h1>
+      <p>Your ultimate site for all 5 prayer times</p>
+      <AthanSearch />
+      {/* <h1 class="text-3xl font-bold underline">
+      Hello world!
+      </h1> */}
+   
+
+    
+    </body>
+
+
+  );
+}
+
+export default App
+
+
